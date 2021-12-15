2a6b5e802f7e345ed112727a83e63fc101049838
Meghan <meghanleighgreen@gmail.com>
2021-12-15 07:15:11 +0000

    reading excel files

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import requests
 import pandas as pd
 
-siteid = 6016527
+siteid = 1016940
+province = "BC"
 # siteid can be found here: https://dd.weather.gc.ca/climate/observations/
 # alternatively, print siteid as table 
-sites = pd.from_csv(("https://dd.weather.gc.ca/climate/observations/climate_station_list.csv")
+sites = pd.read_csv("https://dd.weather.gc.ca/climate/observations/climate_station_list.csv")
 sites.head()
+#print(sites)
 
+siteurl = "https://dd.weather.gc.ca/climate/observations/normals/csv/1981-2010/"+province+"/climate_normals_"+province+"_"+str(siteid)+"_1981-2010.csv"
+print(siteurl)
 
-df = pd.from_csv("https://dd.weather.gc.ca/climate/observations/normals/csv/1981-2010/ON/climate_normals_ON_6016527_1981-2010.csv")
-df.head()
+df = pd.read_csv("siteurl")
+print(df)
+#attempting to save as DataFrame but too much space, trying as regular text file first
 
