2d20e92d45efe3667fcc671420e1a1e434ca844e
Meghan <meghanleighgreen@gmail.com>
2022-01-18 11:33:41 +0000

    nearly finished new file downloading method

--- dailies.py
+++ dailies.py
@@ -9,31 +9,56 @@ from bs4 import BeautifulSoup
 from fetchdata import fetchECCC
 
 
-def collectalldailies(siteID, limit=-1):
+def downloadlist(siteID, yr1, yr2):
+  # builds a list of file names to downbload from ECCC's daily climate data, using yr1,yr2 as an *inclusive* range of years to include
+  prov = findprov(siteID)
+  t = []
+  for yr in range(yr1, yr2+1):
+    for i in range(12): #months
+      fil = "climate_daily_" + prov + "_" + str(siteID) + "_" + str(yr) + "-" + str(i+1).zfill(2) + "_P1D.csv"
+      t.append(fil)
+  return t
+
+#need to add an error message if there's any missing days
+
+
+
+def collectalldailies(siteID, limit=-1, method = 1):
   foldername = "https://dd.weather.gc.ca/climate/observations/daily/csv/" + findprov(
     siteID) + "/"  # string of location of all the files
   filenameprefix = "https://dd.weather.gc.ca/climate/observations/daily/csv/" + findprov(siteID) + "/climate_daily_" + findprov(siteID) + "_" + str(siteID)  #only want files that start with this
 
-  # opening the folder to list out the files
-  folder = requests.get(foldername, timeout=360)
-  soup = BeautifulSoup(folder.text, 'html.parser')
+  if method == 1: 
+    # opening the folder to list out the files
+    folder = requests.get(foldername, timeout=360)
+    soup = BeautifulSoup(folder.text, 'html.parser')
   
-  #fetching the data into a csv-ish list, which writes to file later (currently in main)
-  years = range(1981, 2011) #unclear if assigning it here vs main makes more sense
-  header = []
-  data = []
-  #year = 0000
-  for link in soup.find_all('a'):
-    href = link.get("href")
-    #if len(href)>30 and href.endswith(".csv"): year = int(href[25:29])
-    #print(year, year in years)
-    if siteID in href and href.endswith(".csv") and int(href[25:29]) in years: 
-      print("fetching "+href)
-      d = fetchECCC(foldername + href)
+    #fetching the data into a csv-ish list, which writes to file later (currently in main)
+    years = range(1981, 2011) #unclear if assigning it here vs main makes more sense
+    header = []
+    data = []
+    for link in soup.find_all('a'):
+      href = link.get("href")
+      #if len(href)>30 and href.endswith(".csv"): year = int(href[25:29])
+      #print(year, year in years)
+      if siteID in href and href.endswith(".csv") and int(href[25:29]) in years: 
+        print("fetching "+href)
+        d = fetchECCC(foldername + href)
+        if header == []: header = d[0]
+        data += d[1:]
+    data.insert(0,header)      
+    return data
+
+
+  if method == 2:
+    header = []
+    data = []
+    for item in downloadlist(siteID, 1981, 2021):
+      d = fetchECCC(foldername + item)
       if header == []: header = d[0]
       data += d[1:]
-  data.insert(0,header)      
-  return data
+    data.insert(0,header)      
+    return data
   print("finished collecting")
 
   #dailydata = pd.read_csv(siteIDurl)
