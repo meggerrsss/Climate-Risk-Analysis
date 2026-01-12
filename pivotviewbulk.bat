85512fc95cb0f80a2507a2711ce03b71df0aa623
Meghan <meghanleighgreen@gmail.com>
2026-01-12 15:36:34 -0500

    pivot views

---
+++ pivotviewbulk.bat
@@ -0,0 +1,36 @@
+call conda activate data
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 0
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 1
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 2
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 3
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 4
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 5
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 6
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 7
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 8
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 9
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 10
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 11
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 12
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 13
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 14
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 15
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 16
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 17
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 18
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 19
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 20
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 21
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 22
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 23
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 24
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 25
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 26
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 27
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 28
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 29
+papermill pivotwhisker.ipynb pivotout.ipynb -p increment 30
+
+
+
+call conda deactivate
